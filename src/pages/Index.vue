655ef4f09f4a17c737ea066dfe9c47a186d04fc6
Hossein Nasiri [Linux] <hossein.nasiri.sovari@gmail.com>
2021-03-14 23:56:46 +0330

    First commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,94 @@
+<template>
+  <q-page class="">
+    <div class="flex flex-center q-py-lg">
+        <q-btn label="Connect Serial Device" @click="reqSerialAccess"></q-btn>
+    </div>
+    <div class="row">
+        <div class="col-6 q-mx-auto">
+            <div class="q-mx-auto flex">
+                <q-input v-model="text" label="Write :" class="col-grow"></q-input>
+                <q-btn label="Send Data" class="q-ml-sm q-px-sm" outline :ripple="false"  dense></q-btn>
+            </div>
+        </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+
+  data:()=>({
+    device : null,
+    port : null,
+    baudRate: 9600,
+    connected : false,
+    reader: null,
+    writer: null,
+    text: '',
+  }),
+
+  computed: {
+    filters() {
+        return [
+            { usbVendorId: 0x2341 }
+        ]
+    }
+  },
+
+  methods: {
+    reqSerialAccess(){
+        navigator.serial.requestPort({ filters: this.filters})
+            .then( async (response) =>{
+                this.port = response;
+
+                await this.connectDevice();
+                await this.sayHello();
+                await this.readUntilClosed();
+            })
+
+    },
+
+    async connectDevice(){
+        await this.port.open({
+            baudRate: this.baudRate,
+            dataBits : 8,
+            stopBits: 1,
+            parity: "even",
+            flowControl: "none"
+        })
+
+    },
+
+    async sayHello(){
+        this.writer = await this.port.writable.getWriter();
+        const encoder = new TextEncoder();
+        await this.writer.write(encoder.encode('HELLO'));
+        await this.writer.write(encoder.encode('#'));
+        console.log('writen');
+        this.writer.releaseLock();
+    },
+
+    async readUntilClosed() {
+        const textDecoder = new TextDecoderStream();
+        const readableStreamClosed = this.port.readable.pipeTo(textDecoder.writable);
+        const reader = textDecoder.readable.getReader();
+
+        // Listen to data coming from the serial device.
+        while (true) {
+            console.log('## before read ##');
+          const { value, done } = await reader.read();
+          if (done) {
+            // Allow the serial port to be closed later.
+            reader.releaseLock();
+            break;
+          }
+          // value is a string.
+          console.log(value);
+        }
+
+    }
+
+  }
+}
+</script>
