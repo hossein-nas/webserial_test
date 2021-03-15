a9a6809730d88a827b3b8b84262c62a3b0195843
Hossein Nasiri [Linux] <hossein.nasiri.sovari@gmail.com>
2021-03-15 23:55:22 +0330

    Some updates

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,9 +6,19 @@
     <div class="row">
         <div class="col-6 q-mx-auto">
             <div class="q-mx-auto flex">
-                <q-input v-model="text" label="Write :" class="col-grow"></q-input>
-                <q-btn label="Send Data" class="q-ml-sm q-px-sm" outline :ripple="false"  dense></q-btn>
+                <q-input v-model.trim="command" label="Write :" class="col-grow"></q-input>
+                <q-btn label="Send Data" class="q-ml-sm q-px-sm" outline :ripple="false" @click="triggerSendData" dense></q-btn>
+                <q-btn label="Start Reading" class="q-ml-sm q-px-sm" outline :ripple="false" @click="startReading" dense></q-btn>
             </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6 q-mx-auto">
+            <q-list bordered separator>
+              <q-item clickable v-ripple v-for="(data, ind) in dataList" :key="ind">
+                <q-item-section>{{data}}</q-item-section>
+              </q-item>
+            </q-list>
         </div>
     </div>
   </q-page>
@@ -25,7 +35,10 @@ export default {
     connected : false,
     reader: null,
     writer: null,
-    text: '',
+    command: '',
+    dataList: [
+        'fake DATA 01', 
+    ]
   }),
 
   computed: {
@@ -43,8 +56,6 @@ export default {
                 this.port = response;
 
                 await this.connectDevice();
-                await this.sayHello();
-                await this.readUntilClosed();
             })
 
     },
@@ -60,16 +71,32 @@ export default {
 
     },
 
-    async sayHello(){
+    async triggerSendData(){
+        //transforming string to Uint8Array
+        const data = new Uint8Array(this.command); 
+
+        this.sendData(data); // in this function loginc for sending data with reside;
+    },
+
+    sendData(data){
+        console.log('## sending data ##')
+        console.log("VALUE :", this.command);
+        console.log("ENCODED VALUE :", data);
+
+/*
         this.writer = await this.port.writable.getWriter();
         const encoder = new TextEncoder();
         await this.writer.write(encoder.encode('HELLO'));
         await this.writer.write(encoder.encode('#'));
         console.log('writen');
         this.writer.releaseLock();
+*/
+
     },
 
-    async readUntilClosed() {
+    async startReading() {
+        // HERE reading loginc will reside
+/*        
         const textDecoder = new TextDecoderStream();
         const readableStreamClosed = this.port.readable.pipeTo(textDecoder.writable);
         const reader = textDecoder.readable.getReader();
@@ -86,8 +113,17 @@ export default {
           // value is a string.
           console.log(value);
         }
+*/
 
+        // with this peace you can send data to be shown in list below input 
+        let dataToBeShown = "PLACEHOLDER DATA";
+        this.addDataToConsole(dataToBeShown);
+    },
+
+    addDataToConsole(data){
+        this.dataList.push(data);
     }
+
 
   }
 }
